e04b9584bb1989ef5a56985b0ace76a1b38c7635
marinescuclaudiu <claudiumarinescu@yahoo.com>
2025-01-25 11:40:02 +0200

    Remove useless code

--- backend/src/routes/dbRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { insertPerson, updatePerson, getPerson, deletePerson } from "../controllers/dbController";
-
-export const router = Router();
-
-router.post('/person/add', insertPerson);
-router.put('/person/:id', updatePerson);
-router.get('/person/:id', getPerson);
-router.delete('/person/:id', deletePerson);
