0b5eb1a61e655af61d76f6f94ca769238b68d86e
Laurentiu <laurentiuandoni2001@gmail.com>
2025-01-25 14:44:11 +0200

    change variable names

--- backend/src/controllers/orderController.ts
+++ backend/src/controllers/orderController.ts
@@ -1,81 +1,87 @@
 import { Request, Response } from "express";
 import { pool } from "../config/dbPool";
-import oracledb from 'oracledb';
+import oracledb from "oracledb";
 
 export const addOrder = async (req: Request, res: Response): Promise<any> => {
-    const { id_user, id_cafe, current_date, products } = req.body;
+  const { id_user, id_cafe, current_date, products } = req.body;
+  console.log("🚀 ~ addOrder ~ products:", products);
 
-    console.log('OrderController - Adding order for user ID:', id_user);
-    // Initialize the connection
-    let connection: oracledb.Connection | null = null;
-    
-    try {
-        // Wait for the pool to resolve
-        const resolvedPool = await pool;
+  console.log("OrderController - Adding order for user ID:", id_user);
+  // Initialize the connection
+  let connection: oracledb.Connection | null = null;
 
-        // Get a connection from the pool
-        connection = await resolvedPool.getConnection();
+  try {
+    // Wait for the pool to resolve
+    const resolvedPool = await pool;
 
-        // Prepare the SQL statement
-        const sqlFindId = `SELECT MAX(ID_COMANDA_CLIENT) AS MAX_ID FROM COMANDA_CLIENT`;
+    // Get a connection from the pool
+    connection = await resolvedPool.getConnection();
 
-        // Execute the query
-        const resultFindId = await connection.execute(sqlFindId, [], { outFormat: oracledb.OUT_FORMAT_OBJECT });
-     
-        if (resultFindId.rows && resultFindId.rows.length > 0) {
-            const nextId = (resultFindId.rows[0] as any).MAX_ID + 1;
+    // Prepare the SQL statement
+    const sqlFindId = `SELECT MAX(ID_COMANDA_CLIENT) AS MAX_ID FROM COMANDA_CLIENT`;
 
-            // Prepare the SQL statement
-            const sql = `INSERT INTO COMANDA_CLIENT(ID_COMANDA_CLIENT, ID_CLIENT, ID_CAFENEA, DATA_PLASARII)
+    // Execute the query
+    const resultFindId = await connection.execute(sqlFindId, [], {
+      outFormat: oracledb.OUT_FORMAT_OBJECT,
+    });
+
+    if (resultFindId.rows && resultFindId.rows.length > 0) {
+      const nextId = (resultFindId.rows[0] as any).MAX_ID + 1;
+
+      // Prepare the SQL statement
+      const sql = `INSERT INTO COMANDA_CLIENT(ID_COMANDA_CLIENT, ID_CLIENT, ID_CAFENEA, DATA_PLASARII)
              VALUES(:nextId, :id_user, :id_cafe, :current_date)`;
 
-            // Execute the query
-            const result = await connection.execute(sql, [nextId, id_user, id_cafe, current_date], { autoCommit: true });
+      // Execute the query
+      const result = await connection.execute(
+        sql,
+        [nextId, id_user, id_cafe, current_date],
+        { autoCommit: true }
+      );
 
-            if (result.rowsAffected === 0) {
-                return res.status(404).json({ message: 'Something went wrong' });
-            }
+      if (result.rowsAffected === 0) {
+        return res.status(404).json({ message: "Something went wrong" });
+      }
 
-            // Now iterate over the products array and insert each product
-            for (const product of products) {
-            const { id, quantity, final_price } = product;
+      // Now iterate over the products array and insert each product
+      for (const product of products) {
+        const { id, cantitate, pret } = product;
+        console.log("🚀 ~ addOrder ~ product:", product);
 
-            // Prepare the SQL statement
-            const sql2 = `INSERT INTO COMANDA_PRODUS(ID_COMANDA_CLIENT, ID_PRODUS, CANTITATE, PRET_FINAL)
-            VALUES(:nextId, :id, :quantity, :final_price)`;
+        // Prepare the SQL statement
+        const sql2 = `INSERT INTO COMANDA_PRODUS(ID_COMANDA_CLIENT, ID_PRODUS, CANTITATE, PRET_FINAL)
+            VALUES(:nextId, :id, :cantitate, :pret)`;
 
-            // Execute the query
-            const result2 = await connection.execute(sql2, [nextId, id, quantity, final_price], { autoCommit: true });
-                
-                
-            if (result2.rowsAffected === 0) {
-                return res.status(404).json({ message: 'Something went wrong' });
-            }
-            
-            }
-            return res.status(200).json({ message: 'Order added successfully' });
+        // Execute the query
+        const result2 = await connection.execute(
+          sql2,
+          [nextId, id, cantitate, pret],
+          { autoCommit: true }
+        );
+
+        if (result2.rowsAffected === 0) {
+          return res.status(404).json({ message: "Something went wrong" });
         }
+      }
+      return res.status(200).json({ message: "Order added successfully" });
     }
-    catch (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Internal server error' });
-    } finally {
-        if (connection) {
-            try {
-            await connection.close();
-            } catch (err) {
-            console.error('Error closing connection:', err);
-            }
-        }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error closing connection:", err);
+      }
     }
-}
+  }
+};
 
+export const getOrder = async (req: Request, res: Response): Promise<any> => {};
 
-
-export const getOrder = async (req: Request, res: Response): Promise<any> => {
-    
-}
-
-export const getOrders = async (req: Request, res: Response): Promise<any> => {
-   
-}
+export const getOrders = async (
+  req: Request,
+  res: Response
+): Promise<any> => {};
